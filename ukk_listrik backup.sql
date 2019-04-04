ca065610f1925278486b6f4fa92e656e4e0c4373
crossvalley <23567886+crossvalley@users.noreply.github.com>
2019-04-05 05:36:11 +0700

    Commit v7

--- ukk_listrik backup.sql
+++ ukk_listrik backup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2019 at 12:34 AM
+-- Generation Time: Apr 04, 2019 at 05:57 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -85,7 +85,9 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `username`, `password`, `nomor_kwh`, `nama_pelanggan`, `alamat`, `status_pelanggan`, `id_tarif`) VALUES
+(3, 'user1', '24c9e15e52afc47c225b757e7bee1f9d', 271100, 'Muhammad Rezki Ananda', 'Jalan Danau Ranau G7G1, Sawojajar, Kedungkandang, Kota Malang', 'Aktif', 1),
 (18, 'tester', 'f5d1278e8109edd94e1e4197e04873b9', 121212, 'tester', 'Jalan Gunung Anyar', 'Aktif', 3),
+(21, 'user2', '7e58d63b60197ceb55a1c487989a3720', 1127, 'Rezki Veldora Tempet', 'Dsn. Cangak, Ds. Krandang RT/RW 05/07, Kec. Kras, Kab.Kediri', 'Aktif', 1),
 (22, 'user3', '92877af70a45fd6a2ed7fe81e1236b78', 110027, 'Shino', 'Tokyo, Japan', 'Aktif', 3);
 
 -- --------------------------------------------------------
@@ -111,9 +113,12 @@ CREATE TABLE `pembayaran` (
 --
 
 INSERT INTO `pembayaran` (`id_pembayaran`, `id_tagihan`, `tanggal_pembayaran`, `bulan_bayar`, `biaya_admin`, `total_bayar`, `status`, `bukti`, `id_admin`) VALUES
+(4, 2, '2019-04-01', 'Maret-2022', 2500, 2147483647, 'Lunas', 'goal3.png', 3),
+(5, 3, '2019-04-01', 'Januari-2020', 2500, 36002500, 'Lunas', 'AI1.png', 3),
 (6, 5, '2019-04-01', 'Maret-2019', 2500, 37002500, 'Lunas', 'AI1.png', 3),
 (7, 4, '2019-04-01', 'Januari-2019', 2500, 5002500, 'Lunas', 'AI.png', 3),
-(8, 9, '2019-04-01', 'Agustus-2024', 2500, 74002500, 'Lunas', 'AI1.png', 3);
+(8, 9, '2019-04-01', 'Agustus-2024', 2500, 74002500, 'Lunas', 'AI1.png', 3),
+(9, 10, '2019-04-02', 'Januari-2019', 2500, 3962500, 'Lunas', 'AI1.png', 3);
 
 -- --------------------------------------------------------
 
@@ -135,9 +140,20 @@ CREATE TABLE `penggunaan` (
 --
 
 INSERT INTO `penggunaan` (`id_penggunaan`, `id_pelanggan`, `bulan`, `tahun`, `meter_awal`, `meter_akhir`) VALUES
+(1, 3, 'Februari', 2021, 1000, 5000),
+(2, 3, 'Januari', 2019, 40, 50),
+(3, 3, 'Desember', 2020, 4000, 10000),
+(4, 3, 'Januari', 2021, 40, 80),
+(7, 3, 'Maret', 2022, 1221, 2112121),
+(8, 3, 'Maret', 2022, 1221, 2112121),
+(9, 3, 'Januari', 2020, 1000, 10000),
 (10, 18, 'Januari', 2019, 4000, 9000),
 (11, 18, 'Maret', 2019, 3000, 40000),
-(15, 18, 'Agustus', 2024, 6000, 80000);
+(12, 3, 'Januari', 2019, 3000, 20000),
+(13, 3, 'Februari', 2020, 5000, 900000),
+(14, 3, 'Mei', 2027, 10000, 20000),
+(15, 18, 'Agustus', 2024, 6000, 80000),
+(16, 21, 'Januari', 2019, 10, 1000);
 
 -- --------------------------------------------------------
 
@@ -159,9 +175,15 @@ CREATE TABLE `tagihan` (
 --
 
 INSERT INTO `tagihan` (`id_tagihan`, `id_penggunaan`, `jumlah_meter`, `bulan`, `tahun`, `status`) VALUES
+(2, 8, 2110900, 'Maret', 2022, 'Lunas'),
+(3, 9, 9000, 'Januari', 2020, 'Lunas'),
 (4, 10, 5000, 'Januari', 2019, 'Lunas'),
 (5, 11, 37000, 'Maret', 2019, 'Lunas'),
-(9, 15, 74000, 'Agustus', 2024, 'Lunas');
+(6, 12, 17000, 'Januari', 2019, 'Belum Dibayar'),
+(7, 13, 895000, 'Februari', 2020, 'Belum Dibayar'),
+(8, 14, 10000, 'Mei', 2027, 'Belum Dibayar'),
+(9, 15, 74000, 'Agustus', 2024, 'Lunas'),
+(10, 16, 990, 'Januari', 2019, 'Lunas');
 
 -- --------------------------------------------------------
 
@@ -182,7 +204,7 @@ CREATE TABLE `tarif` (
 --
 
 INSERT INTO `tarif` (`id_tarif`, `nama_tarif`, `daya`, `terperkwh`, `status`) VALUES
-(1, 'TRF001', 1000000, 4000, 'Non Aktif'),
+(1, 'TRF001', 1000000, 4000, 'Aktif'),
 (2, 'TRF002', 2000, 800, 'Aktif'),
 (3, 'TRF003', 5000, 1000, 'Aktif'),
 (6, 'TRF004', 10000, 20000, 'Aktif');
@@ -293,25 +315,25 @@ ALTER TABLE `admin`
 -- Constraints for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`) ON DELETE CASCADE;
+  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`);
 
 --
 -- Constraints for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_tagihan`) REFERENCES `tagihan` (`id_tagihan`) ON DELETE CASCADE;
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_tagihan`) REFERENCES `tagihan` (`id_tagihan`);
 
 --
 -- Constraints for table `penggunaan`
 --
 ALTER TABLE `penggunaan`
-  ADD CONSTRAINT `penggunaan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE;
+  ADD CONSTRAINT `penggunaan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
 
 --
 -- Constraints for table `tagihan`
 --
 ALTER TABLE `tagihan`
-  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`id_penggunaan`) REFERENCES `penggunaan` (`id_penggunaan`) ON DELETE CASCADE;
+  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`id_penggunaan`) REFERENCES `penggunaan` (`id_penggunaan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
